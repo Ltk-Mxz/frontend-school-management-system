2f098e8808725f978bb9acde2d7f4a908d157936
Ltk Mxz <a96.paul96@gmail.com>
2024-09-16 15:07:03 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+import Link from "next/link";
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center gap-4">
+      <Link href="/admin">
+        <span className="text-lg font-semibold text-blue-500 hover:underline">
+          Admin page
+        </span>
+      </Link>
+      <Link href="/student">
+        <span className="text-lg font-semibold text-blue-500 hover:underline">
+          Student page
+        </span>
+      </Link>
+      <Link href="/teacher">
+        <span className="text-lg font-semibold text-blue-500 hover:underline">
+          Teacher page
+        </span>
+      </Link>
+      <Link href="/parent">
+        <span className="text-lg font-semibold text-blue-500 hover:underline">
+          Parent page
+        </span>
+      </Link>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
